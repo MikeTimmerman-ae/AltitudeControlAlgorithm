d17a476f7b654ad28d80a8d453fe2df6b90c1a6f
Mike Timmerman <timmermanmike@hotmail.com>
2022-02-06 22:50:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rocket Altitude Control Algorithm
+Model predictive control algorithm which controls the altitude of a sounding rocket
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <string>
+
+#define PI 3.14159265
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
