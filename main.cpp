32a13082fd11888b369524140d33876edf693e88
Mike Timmerman <timmermanmike@hotmail.com>
2022-02-06 23:03:04 +0100

    add Eigen submodule

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 #include <math.h>
 #include <string>
 
+#include <Eigen/Dense>
+#include <Eigen/SparseCore>
+
 #define PI 3.14159265
 
 using namespace std;
-
+using namespace Eigen;
 
 
 int main(int argc, char const *argv[])
